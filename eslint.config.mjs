25a769977adef430da21b8e76815a37936740823
nithin kumar <nithinkottary25@gmail.com>
2025-05-19 16:04:27 +0530

    add test setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,12 @@ export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
     {
-        ignores: ['dist', 'node_modules', 'eslint.config.mjs', 'jest.config.js'],
+        ignores: [
+            'dist',
+            'node_modules',
+            'eslint.config.mjs',
+            'jest.config.js',
+        ],
     },
     {
         languageOptions: {
@@ -18,7 +23,7 @@ export default tseslint.config(
         },
         rules: {
             'dot-notation': 'error',
-            '@typescript-eslint/no-misused-promises': 'off'
+            '@typescript-eslint/no-misused-promises': 'off',
         },
     },
 )
